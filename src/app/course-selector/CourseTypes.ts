862326854761a46b1e8e43ba4a5732b70025b5af
Julius Gesang <julius@familie-gesang.de>
2024-11-18 13:40:59 +0100

    Add Course Selector

---
+++ src/app/course-selector/CourseTypes.ts
@@ -0,0 +1,20 @@
+export interface Subject {
+  tag: string;
+  de: string;
+  en: string;
+  faculty: Faculty;
+  category: string;
+}
+
+export enum Faculty {
+  Technik = 'Technik',
+  Wirtschaft = 'Wirtschaft',
+  Gesundheit = 'Gesundheit',
+}
+export type FacultySubjects = {[faculty in Faculty]: Subject[]};
+export type SubjectJson = SubjectData[];
+export type SubjectData = {
+  name: string;
+  faculty: string;
+  degreeName: string;
+};
